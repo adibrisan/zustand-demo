c8be76524b518fd334aaa360451761f4684ffe55
adibrisan <adi.brisan@yahoo.com>
2024-01-11 12:51:50 +0200

    Delete tasks with zustand store

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,4 +1,5 @@
 import { useStore } from "../store";
+import trash from "../assets/trash-2.svg";
 
 import styles from "./Task.module.css";
 
@@ -6,12 +7,20 @@ const Task = ({ title }) => {
   const task = useStore((store) =>
     store.tasks.find((task) => task.title === title)
   );
+  const deleteTask = useStore((store) => store.deleteTask);
+
   const combinedClass = `${styles.status} ${styles[task.state]}`;
   return (
     <div className={styles.task}>
       <div>{task.title}</div>
       <div className={styles.bottomWrapper}>
-        <div></div>
+        <div>
+          <img
+            src={trash}
+            alt="trash icon"
+            onClick={() => deleteTask(task.title)}
+          />
+        </div>
         <div className={combinedClass}>{task.state}</div>
       </div>
     </div>
